19ed5ed9271bda179ede65602810604094e6347e
Jan Beránek <86586714+jan-beranek@users.noreply.github.com>
2023-10-25 08:36:36 +0200

    feat: TestWarez contest form included

--- src/pages/testwarez/index.tsx
+++ src/pages/testwarez/index.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 const TestWarez = () => {
-  return <div>Hello, I'm TestWarez page!</div>;
+  return (
+    <div>Wopee.io at TestWarez!</div>
+    <script charset="utf-8" type="text/javascript" src="//js-eu1.hsforms.net/forms/embed/v2.js"></script>
+    <script>
+      hbspt.forms.create({
+        region: "eu1",
+        portalId: "139620033",
+        formId: "934d4de3-0bcf-45d5-8590-d8c31e861c8d"
+      });
+    </script>
+    );
 };
 export default TestWarez;
