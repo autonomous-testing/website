1f237fa2a568b5b8fbabdd4c8de8cd6e125fe1c4
Sota Giorgadze <giorgadze@inbox.lv>
2023-12-01 12:38:25 +0100

    refactor: update team page
    migrate team to about us path
    redirect the old page to the new one
    add section with info about our company and investors
    adjust carousel component and reuse it in about us page

--- src/pages/billing/index.tsx
+++ src/pages/billing/index.tsx
@@ -1,19 +1,10 @@
 import React, { useEffect } from "react";
 
 const Billing = () => {
-  const [isMounted, setIsMounted] = React.useState(false);
-
   useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  useEffect(() => {
-    if (!isMounted) {
-      return;
-    }
     window.location.href =
       "https://billing.stripe.com/p/login/5kA4hV8Qw2Od71m4gg";
-  }, [isMounted]);
+  }, []);
 
   return <></>;
 };
