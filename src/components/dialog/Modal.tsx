1247787249327e21071a2698bd36bcb3d544109b
Sota Giorgadze <giorgadze@inbox.lv>
2023-12-04 10:51:39 +0100

    feat: add modal with fullscreen image on image thumbnail click
    make fully responsive layout
    disable modal on screen width below 640px

---
+++ src/components/dialog/Modal.tsx
@@ -0,0 +1,63 @@
+import clsx from "clsx";
+import React, { useEffect, useRef, type ComponentPropsWithoutRef } from "react";
+
+export type ModalProps = ComponentPropsWithoutRef<"dialog"> & {
+  onClose: () => void;
+  chosenImageSrc?: string;
+};
+
+export default function Modal(props: ModalProps) {
+  const { children, open, onClose, className, chosenImageSrc, ...rest } = props;
+  const ref = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    const dialog = ref.current!;
+    if (open) {
+      dialog.showModal();
+      dialog.dataset.open = "";
+    } else {
+      delete dialog.dataset.open;
+      const handler = () => dialog.close();
+      const inner = dialog.children[0] as HTMLElement;
+      inner.addEventListener("transitionend", handler);
+      return () => inner.removeEventListener("transitionend", handler);
+    }
+  }, [open]);
+
+  useEffect(() => {
+    const dialog = ref.current!;
+    const handler = (e: Event) => {
+      e.preventDefault();
+      onClose();
+    };
+    dialog.addEventListener("close", handler);
+    dialog.addEventListener("cancel", handler);
+    return () => {
+      dialog.removeEventListener("close", handler);
+      dialog.removeEventListener("cancel", handler);
+    };
+  }, [onClose]);
+
+  return (
+    <dialog
+      ref={ref}
+      className={clsx("group", className)}
+      {...rest}
+    >
+      <div
+        className="fixed inset-0 px-4 xl:px-0 flex items-center justify-center bg-black/75 opacity-0 transition-all group-data-[open]:opacity-100"
+        onClick={onClose}
+      >
+        <div className="max-w-7xl flex overflow-hidden rounded-xl scale-75 bg-white opacity-0 transition-all group-data-[open]:scale-100 group-data-[open]:opacity-100">
+          {chosenImageSrc && (
+            <img
+              src={chosenImageSrc}
+              alt="picture"
+            />
+          )}
+          {children}
+        </div>
+      </div>
+    </dialog>
+  );
+}
--- src/pages/wopee-bot/index.tsx
+++ src/pages/wopee-bot/index.tsx
@@ -1,23 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { useWindowSize } from "usehooks-ts";
 
 import Layout from "@theme/Layout";
+import Modal from "@site/src/components/dialog/Modal";
 
-const cmdPictures = [
-  {
-    alt: "Dashboard",
-    src: "/img/cmd/dashboard.jpg",
-  },
-  {
-    alt: "Scenarios",
-    src: "/img/cmd/scenarios.jpg",
-  },
-  {
-    alt: "VvdPopUp",
-    src: "/img/cmd/vvd.jpg",
-  },
-];
+const WopeeBotPage = () => {
+  const [open, setOpen] = useState(false);
+  const [chosenImageSrc, setChosenImageSrc] = useState(null);
 
-const TeamPage = () => {
+  const { width } = useWindowSize();
+
   return (
     <Layout>
       <main className="mt-8 mb-16 container">
@@ -25,74 +17,115 @@ const TeamPage = () => {
           Autonomous Testing Bot
         </h2>
 
-        <div className="text-justify flex flex-col gap-2 my-10 max-w-5xl mx-auto">
-          <p className="text-left">
-            Our Autonomous Testing Bot revolutionizes the testing process for
-            your application.
-          </p>
-          <p>
-            Wopee Bot conducts a comprehensive scan of your application,
-            gathering vital information about potential interactions and
-            capturing screenshots. This information is then used to
-            automatically generate tests, serving as assertions for your
-            application. The generated tests can be prioritized, allowing for
-            seamless regression testing.
-          </p>
-          <p>
-            Unlike traditional testing methods, there's no need for manual test
-            case preparation or coding. The entire testing process is automated,
-            making it a hassle-free experience for you. Learning programming
-            languages or utilizing record-and-play tools is unnecessary.
-          </p>
-          <p>
-            Maintenance is also simplified. When your application undergoes
-            changes, you receive a report highlighting the modifications that
-            need assessment. For bug reporting, we follow standard procedures.
-            For new application versions, a simple one-click updates the
-            application testing model.
-          </p>
-          <p>
-            The validation process for the application under test relies heavily
-            on visual examination. In addition to the visual inspection, we
-            meticulously gather data on various aspects such as HTTP status
-            codes, JavaScript errors, and the speed at which the application
-            loads. These comprehensive quality metrics serve as crucial
-            indicators, allowing us to identify and address potential issues
-            with the application under test.
-          </p>
-        </div>
+        <div className="text-justify flex flex-col gap-20 my-10 mx-auto">
+          <div className="flex flex-col-reverse gap-10 xl:flex-row xl:gap-0 items-center">
+            <div className="flex flex-1 flex-col gap-5 xl:text-xl">
+              <p className="text-left px-0 xl:px-12">
+                Our Autonomous Testing Bot revolutionizes the testing process
+                for your application.
+              </p>
+              <p className="px-0 xl:px-12">
+                Wopee Bot conducts a comprehensive scan of your application,
+                gathering vital information about potential interactions and
+                capturing screenshots. This information is then used to
+                automatically generate tests, serving as assertions for your
+                application. The generated tests can be prioritized, allowing
+                for seamless regression testing.
+              </p>
+            </div>
 
-        <div className="flex flex-col md:flex-row justify-center gap-10 my-10 xl:my-16">
-          {cmdPictures.map((picture) => (
-            <div>
+            <div className="flex-1 flex justify-center">
               <img
-                className="rounded-lg object-contain shadow-2xl"
-                src={picture.src}
-                alt={picture.alt}
+                onClick={() => {
+                  if (width < 640) return;
+                  setOpen(true);
+                  setChosenImageSrc("/img/cmd/dashboard.jpg");
+                }}
+                className="rounded-lg object-contain h-auto w-full xl:w-auto xl:h-80 shadow-2xl sm:hover:cursor-pointer"
+                src="/img/cmd/dashboard.jpg"
+                alt="dashboard"
               />
             </div>
-          ))}
+          </div>
+
+          <div className="flex flex-col gap-10 xl:flex-row xl:gap-0 items-center">
+            <div className="flex-1 flex justify-center">
+              <img
+                onClick={() => {
+                  if (width < 640) return;
+                  setOpen(true);
+                  setChosenImageSrc("/img/cmd/scenarios.jpg");
+                }}
+                className="rounded-lg object-contain h-auto w-full xl:w-auto xl:h-80 shadow-2xl sm:hover:cursor-pointer"
+                src="/img/cmd/scenarios.jpg"
+                alt="scenarios"
+              />
+            </div>
+            <div className="flex flex-1 flex-col gap-5 xl:text-xl">
+              <p className="px-0 xl:px-12">
+                Unlike traditional testing methods, there's no need for manual
+                test case preparation or coding. The entire testing process is
+                automated, making it a hassle-free experience for you. Learning
+                programming languages or utilizing record-and-play tools is
+                unnecessary.
+              </p>
+              <p className="px-0 xl:px-12">
+                Maintenance is also simplified. When your application undergoes
+                changes, you receive a report highlighting the modifications
+                that need assessment. For bug reporting, we follow standard
+                procedures. For new application versions, a simple one-click
+                updates the application testing model.
+              </p>
+            </div>
+          </div>
+
+          <div className="flex flex-col-reverse gap-10 xl:flex-row xl:gap-0 items-center">
+            <div className="flex-1 xl:text-xl flex flex-col gap-5">
+              <p className="px-0 xl:px-12">
+                The validation process for the application under test relies
+                heavily on visual examination. In addition to the visual
+                inspection, we meticulously gather data on various aspects such
+                as HTTP status codes, JavaScript errors, and the speed at which
+                the application loads. These comprehensive quality metrics serve
+                as crucial indicators, allowing us to identify and address
+                potential issues with the application under test.
+              </p>
+              <div className="px-0 xl:px-12">
+                <h4 className="text-xl text-secondary-wopee dark:text-primary-wopee">
+                  Benefits:
+                </h4>
+                <ul className="text-left">
+                  <li>
+                    Hassle-free automation without manual test case preparation.
+                  </li>
+                  <li>
+                    No need for programming skills or record-and-play tools.
+                  </li>
+                  <li>Seamless regression testing with prioritized tests.</li>
+                  <li>
+                    Simplified maintenance with automatic reporting of
+                    application changes.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex-1 flex justify-center">
+              <img
+                onClick={() => {
+                  if (width < 640) return;
+                  setOpen(true);
+                  setChosenImageSrc("/img/cmd/vvd.jpg");
+                }}
+                className="rounded-lg object-contain h-auto w-full xl:w-auto xl:h-80 shadow-2xl sm:hover:cursor-pointer"
+                src="/img/cmd/vvd.jpg"
+                alt="vvdPopUp"
+              />
+            </div>
+          </div>
         </div>
 
-        <div className="text-justify flex flex-col gap-2 my-10 max-w-5xl mx-auto">
-          <h4 className="text-2xl text-secondary-wopee dark:text-primary-wopee">
-            Benefits:
-          </h4>
-          <ul>
-            <li>
-              Hassle-free automation without manual test case preparation.
-            </li>
-            <li>No need for programming skills or record-and-play tools.</li>
-            <li>Seamless regression testing with prioritized tests.</li>
-            <li>
-              Simplified maintenance with automatic reporting of application
-              changes.
-            </li>
-          </ul>
-        </div>
-
-        <div className="text-justify flex flex-col gap-2 mt-10 max-w-5xl mx-auto">
-          <h3 className="text-2xl text-secondary-wopee dark:text-primary-wopee">
+        <div className="text-justify flex flex-col gap-4 mt-10">
+          <h3 className="xl:text-4xl text-center text-secondary-wopee dark:text-primary-wopee">
             Frequently Asked Questions
           </h3>
           <div className="my-2 flex flex-col gap-5">
@@ -185,8 +218,14 @@ const TeamPage = () => {
             </div>
           </div>
         </div>
+
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+          chosenImageSrc={chosenImageSrc}
+        />
       </main>
     </Layout>
   );
 };
-export default TeamPage;
+export default WopeeBotPage;
