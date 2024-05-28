97de4c46f78b0eff701a60475cd239a142e8a8ab
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2024-05-28 05:38:45 +0200

    feat: add Oriflame ws page

---
+++ src/pages/240528-oriflame/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const Oriflame = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://gist.github.com/jan-beranek/7b9fee65924fd3212723506700216a74";
+  }, []);
+
+  return <></>;
+};
+export default Oriflame;
