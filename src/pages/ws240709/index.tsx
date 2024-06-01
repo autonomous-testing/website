47c385441a4faf6108ea8301f92d397c5b9a323c
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2024-06-01 11:08:27 +0200

    feat: add 07 and 08 ws

---
+++ src/pages/ws240709/index.tsx
@@ -0,0 +1,68 @@
+import { Helmet } from "react-helmet";
+import React, { useLayoutEffect, useState } from "react";
+
+import GoBack from "@site/src/components/demo/GoBack";
+import Loader from "@site/src/components/demo/Loader";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import PartnersCarousel from "@site/src/components/demo/PartnersCarousel";
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    const script = document.createElement("script");
+    script.src = "//js-eu1.hsforms.net/forms/embed/v2.js";
+    script.charset = "utf-8";
+    script.type = "text/javascript";
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      setTimeout(() => {
+        if (window.hbspt) {
+          window.hbspt.forms.create({
+            region: "eu1",
+            portalId: "139620033",
+            formId: "24e1b1e0-eb98-468a-a615-e96a5d77a1f1",
+            target: "#hubspot-form",
+          });
+        }
+        setIsLoading(false);
+      }, 100);
+    };
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>{siteConfig.title}</title>
+        <meta
+          name="description"
+          content="Autonomous testing platform for web applications | Wopee.io"
+        />
+        <script
+          type="text/javascript"
+          src="https://static.hsappstatic.net/MeetingsEmbed/ex/MeetingsEmbedCode.js"
+        ></script>
+      </Helmet>
+      <div className="container">
+        <GoBack />
+        <div className="flex items-center justify-center my-5 xl:items-start xl:flex-row gap-5">
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <div className="w-full xl:w-1/2 py-4">
+              <div id="hubspot-form" />
+            </div>
+          )}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
