53d2cb9fdc153bb44a0e6aa8d9aab3edfd14e52b
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2024-09-27 11:02:18 +0200

    fix: newsletter form heigh

--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -11,7 +11,7 @@ const NewsletterPage = () => {
             {/* <h1>Newsletter</h1>
             <p>Subscribe to our newsletter to get the latest updates.</p>
             <div id="newsletter-form-container"></div> */}
-            <iframe width="540" height="305" src="https://02c03965.sibforms.com/serve/MUIFAPMLWr6ANLZ18gyb0EOpklxMm2lUohur7eBpm51aVFk2kE0pCiZYju_sdgnW-k1DnV9IsOloZChjjwMdumSnqCjZEZS-t-JvdEnubIxtKYl6rk7xqBwkPV7KF0HV-PVXr8C9nMY5_D8e7VY-bcDfYCXL-hcSMLtAm6-hBl0XHqN6WCvYFNecgwbcSbxbUdp-19PfbRdlNSl9" frameborder="0" scrolling="auto" allowfullscreen></iframe>
+            <iframe width="540" height="605" src="https://02c03965.sibforms.com/serve/MUIFAPMLWr6ANLZ18gyb0EOpklxMm2lUohur7eBpm51aVFk2kE0pCiZYju_sdgnW-k1DnV9IsOloZChjjwMdumSnqCjZEZS-t-JvdEnubIxtKYl6rk7xqBwkPV7KF0HV-PVXr8C9nMY5_D8e7VY-bcDfYCXL-hcSMLtAm6-hBl0XHqN6WCvYFNecgwbcSbxbUdp-19PfbRdlNSl9" frameborder="0" scrolling="auto" allowfullscreen></iframe>
           </div>
         </div>
       </main>
