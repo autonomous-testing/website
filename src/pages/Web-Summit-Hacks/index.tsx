4108d6dac13db526c50eedb5a06c86ff7d3c9c26
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2024-11-04 07:35:14 +0100

    feat: add web summit hacks redirect

---
+++ src/pages/Web-Summit-Hacks/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const WebSummitHacks = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://medium.com/cxo-in-startup/12-crazy-networking-hacks-web-summit-2024-lisbon-9a27ea7ef253?sk=1d3e02f7607aa37e0d317218269aa55c";
+  }, []);
+
+  return <></>;
+};
+export default WebSummitHacks;
