87b32ded43d4f2c35b6403cf19545cda4f0e71e5
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2024-11-06 15:50:59 +0100

    feat: add link to nowork book

---
+++ src/pages/NoWork/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const WebSummitHacks = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://airtable.com/app5STkTzwEJAahEf/shrZYrGLHzIJ7VMMm";
+  }, []);
+
+  return <></>;
+};
+export default WebSummitHacks;
