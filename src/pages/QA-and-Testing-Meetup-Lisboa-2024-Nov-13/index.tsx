84c22554a6106b77bec52ae1a291f9a50beeb218
jan-beranek <86586714+jan-beranek@users.noreply.github.com>
2024-11-11 08:06:39 +0000

    feat: add ws meetup link

---
+++ src/pages/QA-and-Testing-Meetup-Lisboa-2024-Nov-13/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+const WebSummitTesting = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://www.linkedin.com/events/qa-testingmeetup7260689831461113856/";
+  }, []);
+
+  return <></>;
+};
+export default WebSummitTesting;
