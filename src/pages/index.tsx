c9532cb1f586393c8ce6d77dd0dabe7c29288f7d
Marcel Veselka <marcel-veselka@users.noreply.github.com>
2025-07-11 10:50:00 +0200

    Feat/testimonials and social proof (#116)
    * feat: add HomeSocialProof component to the homepage
    
    * feat: add HomeTestingFrameworks component to the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import Layout from "@theme/Layout";
 
 import HomeHeroVibe from "@/components/home-page/HomeHeroVibe";
+import HomeSocialProof from "@/components/home-page/HomeSocialProof";
 import HomeBenefits from "@/components/home-page/HomeBenefits";
+import HomeTestingFrameworks from "@/components/home-page/HomeTestingFrameworks";
 import HomeTrustedBy from "@/components/home-page/HomeTrustedBy";
 import HomeProductCards from "@/components/home-page/HomeProductCards";
 import HomeEndingSection from "@/components/home-page/HomeEndingSection";
@@ -12,7 +14,9 @@ export default function Home(): JSX.Element {
   return (
     <Layout>
       <HomeHeroVibe />
+      <HomeSocialProof />
       <HomeBenefits />
+      <HomeTestingFrameworks />
       <HomeProductCards />
       <HomeTrustedBy />
       <HomeEndingSection />
